547856d869922f3e58524893f9d4012e3357bb27
Riya Sunar <137243584+Riya207@users.noreply.github.com>
2024-05-29 00:31:53 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// HOME SWIPER
+var swiper new Swiper(".mySwiper", {
+          spaceBetween: 85,
+          pagination: {
+                    el: ".swiper-pagination", clickable:true
+          }
+});
